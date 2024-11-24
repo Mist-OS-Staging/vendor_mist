c0b95769137f304f2ad597d86adde99a515e6ff8
ShukakuZa <yusufzuzu787@gmail.com>
2024-11-24 22:35:28 +0530

    vendor: fix mist version

--- build/tasks/otapackage.mk
+++ build/tasks/otapackage.mk
@@ -17,7 +17,7 @@
 # -----------------------------------------------------------------
 # MistOS OTA update package
 
-MIST_TARGET_PACKAGE := $(PRODUCT_OUT)/MistOS-$(MIST_BUILD_VERSION)-ota.zip
+MIST_TARGET_PACKAGE := $(PRODUCT_OUT)/MistOS-$(MIST_BUILD_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
@@ -27,9 +27,6 @@ bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) $(SHA256) $(MIST_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(MIST_TARGET_PACKAGE).sha256sum
 	$(hide) ./vendor/mist/build/tasks/ascii_output.sh
 	@echo ""
-	@echo "Creating json OTA..." >&2
-	$(hide) ./vendor/mist/build/tools/createjson.sh $(TARGET_DEVICE) $(PRODUCT_OUT) MistOS-$(MIST_BUILD_VERSION)-ota.zip $(MIST_VERSION) $(MIST_CODENAME) $(MIST_PACKAGE_TYPE) $(MIST_RELEASE_TYPE)
-	$(hide) cp -f $(PRODUCT_OUT)/$(MIST_PACKAGE_TYPE)_$(TARGET_DEVICE).json vendor/mistOTA/$(MIST_PACKAGE_TYPE)_$(TARGET_DEVICE).json
 	@echo ":·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·::·.·.·:" >&2
 	@echo " Size            : $(shell du -hs $(MIST_TARGET_PACKAGE) | awk '{print $$1}')"
 	@echo " Size(in bytes)  : $(shell wc -c $(MIST_TARGET_PACKAGE) | awk '{print $$1}')"
