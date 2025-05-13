3e0f7cd509104f85becf8c390dafa3173cac64f3
Pranav Vashi <neobuddy89@gmail.com>
2025-05-13 08:30:12 +0000

    mist: Enable Cloned App Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/mist.mk
+++ config/mist.mk
@@ -1,4 +1,9 @@
 
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-mist-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-mist-product.xml
+
+# MistOS Props
 ifeq ($(MIST_BUILD_TYPE),OFFICIAL)
 PRODUCT_PACKAGES += \
     Updater
