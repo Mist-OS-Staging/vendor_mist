b31f477ed1a664f43ec31cdee65fd4c4941e0671
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-13 08:54:01 +0000

    Add option to ship viperfx Change-Id: I35cea2206d098a53e03c54f462b59db991c891bb Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -3,6 +3,11 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product, vendor/lineage/config/mist.mk)
 $(call inherit-product, vendor/extras/mist.mk)
 
+# VIPERFX
+ifeq ($(TARGET_INCLUDE_VIPERFX),true)
+$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
+endif
+
 PRODUCT_BRAND ?= MistOS
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
