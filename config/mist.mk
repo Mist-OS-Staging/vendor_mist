0c0975a3489c5224b32d068f865d45d85dcb5bfa
minaripenguin <minaripenguin@users.noreply.github.com>
2025-05-13 08:47:25 +0000

    config: Build Updater for all * Local update could still be helpful

--- config/mist.mk
+++ config/mist.mk
@@ -60,14 +60,12 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-mist-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-mist-product.xml
 
 # MistOS Props
-ifeq ($(MIST_BUILD_TYPE),OFFICIAL)
 PRODUCT_PACKAGES += \
     Updater
 
 ifeq ($(WITH_GMS),false)
 PRODUCT_PACKAGES += \
     UpdaterVanillaOverlay
-endif
 endif
 
 # Disable async MTE on a few processes
