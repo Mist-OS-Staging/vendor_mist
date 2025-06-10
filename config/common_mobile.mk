00a46195ca6f3b69de60c013f4a937119576d217
minaripenguin <minaripenguin@users.noreply.github.com>
2025-06-10 17:35:29 +0000

    Remove lineage audio prebuilts Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -2,11 +2,8 @@
 $(call inherit-product, vendor/lineage/config/common.mk)
 
 # Include AOSP audio files
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
 include vendor/lineage/config/aosp_audio.mk
 
-# Include Lineage audio files
-include vendor/lineage/audio/audio.mk
 
 # Apps
 PRODUCT_PACKAGES += \
