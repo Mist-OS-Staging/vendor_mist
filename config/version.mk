64ea5496ff863b1adf863c2c26ce3c943a249190
Zabuka_zuzu <zuzu950444@gmail.com>
2025-06-13 19:03:52 +0000

    vendor: use proper version to show PICO|MINI|VANILLA build

--- config/version.mk
+++ config/version.mk
@@ -8,17 +8,24 @@ MIST_VERSION_BASE := 3.5
 MIST_CODENAME := Drizzle
 MIST_BUILD_TYPE ?= Unofficial
 
+# Mist Packages
 ifeq ($(WITH_GMS),true)
-MIST_VERSION := $(MIST_VERSION_BASE)-GApps
+  ifeq ($(TARGET_USES_MINI_GAPPS), true)
+    MIST_PACKAGE_TYPE ?= MINI
+  else ifeq ($(TARGET_USES_PICO_GAPPS), true)
+    MIST_PACKAGE_TYPE ?= PICO
+  else
+    MIST_PACKAGE_TYPE ?= GAPPS
+  endif
 else
-MIST_VERSION := $(MIST_VERSION_BASE)-Vanilla
+  MIST_PACKAGE_TYPE ?= VANILLA
 endif
 
 # Internal version
-LINEAGE_VERSION := MistOS-$(MIST_VERSION)-$(MIST_CODENAME)-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-$(MIST_BUILD_TYPE)
+LINEAGE_VERSION := MistOS-$(MIST_VERSION_BASE)-$(MIST_CODENAME)-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(MIST_PACKAGE_TYPE)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-$(MIST_BUILD_TYPE)
 
 # Display version
-LINEAGE_DISPLAY_VERSION := MistOS-$(MIST_VERSION)-$(MIST_CODENAME)-$(LINEAGE_BUILD)-$(MIST_BUILD_TYPE)-$(shell date +%Y%m%d)
+LINEAGE_DISPLAY_VERSION := MistOS-$(MIST_VERSION_BASE)-$(MIST_CODENAME)-$(MIST_PACKAGE_TYPE)-$(LINEAGE_BUILD)-$(MIST_BUILD_TYPE)-$(shell date +%Y%m%d)
 
 # LineageOS version properties
 PRODUCT_SYSTEM_PROPERTIES += \
@@ -26,6 +33,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.mist.display.version=$(LINEAGE_DISPLAY_VERSION) \
     ro.mist.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
     ro.modversion=$(MIST_VERSION) \
+    ro.mist.packagetype=$(MIST_PACKAGE_TYPE) \
     ro.mist.version_display=$(MIST_VERSION_DISPLAY) \
     ro.mist.version.base=$(MIST_VERSION_BASE) \
     ro.mistos.maintainer=$(MISTOS_MAINTAINER) \
