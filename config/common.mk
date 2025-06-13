ca1b5051c269783ac759db0c1ae8fb3728f8a14a
Alvin Francis <nivlafx@gmail.com>
2025-06-13 18:11:15 +0000

    etc: Add bootanim init Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -150,14 +150,12 @@ $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(LINEAGE_BUI
 endif
 
 # Bootanimation
-TARGET_INCLUDE_BOOT_ANIMATIONS ?= true
-ifeq ($(TARGET_INCLUDE_BOOT_ANIMATIONS),true)
+include vendor/lineage/config/bootanimation.mk
 include vendor/lineage/bootanimation/prebuilts/prebuilts.mk
-endif
-TARGET_SCREEN_WIDTH ?= 1080
-TARGET_SCREEN_HEIGHT ?= 1920
-PRODUCT_PACKAGES += \
-    bootanimation.zip
+
+# BootanimAdd commentMore actions
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/init/init.bootanim.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.bootanim.rc
 
 # Build Manifest
 PRODUCT_PACKAGES += \
