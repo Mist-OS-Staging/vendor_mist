6fe0c3c53bf32a85045934057bd16c297c4ccb85
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-23 20:19:47 +0000

    config: add blur prop support by default this allows users to enable/disable window level blur, the global window blur enablement will still rely on ro.custom.blur.enable value
    Change-Id: I5fabdbbdfe812346f17dbba5d4bf9abe7344e0ca

--- config/mist.mk
+++ config/mist.mk
@@ -32,6 +32,8 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.custom.blur.enable=false
 endif
 
+PRODUCT_SYSTEM_PROPERTIES += ro.surface_flinger.supports_background_blur=1
+
 # DeviceAsWebcam
 ifeq ($(TARGET_BUILD_DEVICE_AS_WEBCAM), true)
     PRODUCT_PACKAGES += \
