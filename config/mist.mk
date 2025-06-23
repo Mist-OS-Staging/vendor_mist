856886cfacb4a43360958e457ed8a8e5efba58ce
minaripenguin <minaripenguin@users.noreply.github.com>
2025-06-23 19:45:53 +0000

    config: Disable memtag for different processes * it defaults to async if not specified
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/mist.mk
+++ config/mist.mk
@@ -36,3 +36,10 @@ ifeq ($(WITH_GMS),true)
     endif
   endif
 endif
+
+# Disable async MTE on a few processes
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    persist.arm64.memtag.app.com.android.se=off \
+    persist.arm64.memtag.app.com.google.android.bluetooth=off \
+    persist.arm64.memtag.app.com.android.nfc=off \
+    persist.arm64.memtag.process.system_server=off
