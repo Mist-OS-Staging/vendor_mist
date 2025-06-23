ca1d46744fe9b2eb90712e8f2f10e3429c0d817a
Dyneteve <dyneteve@hentaios.com>
2025-06-23 19:14:18 +0000

    vendor: Inherit Pixel additions via WITH_GMS * GMS * Audio * Charger * RRO overlays Co-authored-by: someone5678 <nemui3353@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -3,6 +3,12 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product, vendor/lineage/config/mist.mk)
 $(call inherit-product, vendor/extras/mist.mk)
 
+# Pixel additions
+ifeq ($(WITH_GMS),true)
+$(call inherit-product, vendor/google/overlays/ThemeIcons/config.mk)
+$(call inherit-product, vendor/pixel-style/config/common.mk)
+endif
+
 PRODUCT_BRAND ?= MistOS
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
@@ -149,7 +155,6 @@ PRODUCT_COPY_FILES += \
 
 # Config
 PRODUCT_PACKAGES += \
-    SimpleDeviceConfig \
     SimpleSettingsConfig
 
 # Extra tools in Lineage
@@ -205,8 +210,10 @@ PRODUCT_PACKAGES += \
     rsync
 
 # Storage manager
+ifeq ($(WITH_GMS),false)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.storage_manager.enabled=true
+endif
 
 # These packages are excluded from user builds
 PRODUCT_PACKAGES_DEBUG += \
@@ -242,11 +249,6 @@ ifeq ($(TARGET_BUILD_VARIANT),userdebug)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     debug.sf.enable_transaction_tracing=false
 endif
-
-# SetupWizard
-PRODUCT_PRODUCT_PROPERTIES += \
-    setupwizard.theme=glif_v4 \
-    setupwizard.feature.day_night_mode_enabled=true
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/no-rro
 PRODUCT_PACKAGE_OVERLAYS += \
