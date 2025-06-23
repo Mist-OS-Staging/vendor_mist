7ac9b2a7b4cc447180bfb3234b72f1ba5aeeacee
Pranav Vashi <neobuddy89@gmail.com>
2025-06-23 20:01:10 +0000

    config: Build BtHelper

--- config/mist.mk
+++ config/mist.mk
@@ -2,6 +2,10 @@
 PRODUCT_PACKAGES += \
     GameSpace
 
+# BtHelper
+PRODUCT_PACKAGES += \
+    BtHelper
+
 # DeviceAsWebcam
 ifeq ($(TARGET_BUILD_DEVICE_AS_WEBCAM), true)
     PRODUCT_PACKAGES += \
