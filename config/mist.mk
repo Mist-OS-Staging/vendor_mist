95f9bcbb64b080af2e36f2c3dc956270c6933dde
ShukakuZa <yusufzuzu787@gmail.com>
2025-08-23 11:02:41 +0000

    config: pihooks: Update to Baklava QPR2 Beta 1

--- config/mist.mk
+++ config/mist.mk
@@ -2,17 +2,17 @@
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_MANUFACTURER?=Google \
     persist.sys.pihooks_BRAND?=google \
-    persist.sys.pihooks_PRODUCT?=tangorpro_beta \
-    persist.sys.pihooks_DEVICE?=tangorpro \
-    persist.sys.pihooks_ID?=BP31.250523.006 \
+    persist.sys.pihooks_PRODUCT?=cheetah_beta \
+    persist.sys.pihooks_DEVICE?=cheetah \
+    persist.sys.pihooks_ID?=BP41.250725.006 \
     persist.sys.pihooks_RELEASE?=12 \
-    persist.sys.pihooks_SECURITY_PATCH?=2025-05-05 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-08-05 \
     persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
     persist.sys.pihooks_SDK_INT?=32
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PihooksGmsFp="google/tangorpro_beta/tangorpro:16/BP31.250523.006/13607978:user/release-keys" \
-    PihooksGmsModel="Pixel Tablet"
+    PihooksGmsFp="google/cheetah_beta/cheetah:Baklava/BP41.250725.006/13939570:user/release-keys" \
+    PihooksGmsModel="Pixel 7 Pro"
 
 ifeq ($(TARGET_INCLUDE_VIPERFX),true)
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
