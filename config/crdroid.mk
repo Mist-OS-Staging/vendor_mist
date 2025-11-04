b89ff5fcbf8b6b59481d156e6c79960ad0ce25de
Joey <joey@evolution-x.org>
2025-11-04 19:18:26 +0000

    config: Update pihooks fp to A16 QPR2 Beta 3.1

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -1,3 +1,20 @@
+# PIF values
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_MANUFACTURER?=Google \
+    persist.sys.pihooks_BRAND?=google \
+    persist.sys.pihooks_PRODUCT?=comet_beta \
+    persist.sys.pihooks_DEVICE?=comet \
+    persist.sys.pihooks_ID?=BP41.250916.010.A1 \
+    persist.sys.pihooks_RELEASE?=12 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-10-05 \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_SDK_INT?=32
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildFingerprint=google/comet_beta/comet:16/BP41.250916.010.A1/14281945:user/release-keys \
+    PihooksGmsFp="google/comet_beta/comet:16/BP41.250916.010.A1/14281945:user/release-keys" \
+    PihooksGmsModel="Pixel 9 Pro Fold"
+
 # Additional props
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.debug.alloc=0 \
