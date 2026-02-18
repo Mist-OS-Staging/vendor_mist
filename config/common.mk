4a0e44f05ea90541bcfd00e5402a8830e3d8216b
Pranav Vashi <neobuddy89@gmail.com>
2026-02-18 20:23:15 +0000

    build: Ship as MistOS based on LOS Change-Id: I53c407da455885af7029853ff635090f34b01f0c Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,9 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
+$(call inherit-product-if-exists, vendor/lineage/config/mist.mk)
+$(call inherit-product-if-exists, vendor/extras/config.mk)
+$(call inherit-product-if-exists, vendor/extra/product.mk)
+$(call inherit-product-if-exists, vendor/fontbox/config.mk)
 
 # Exclude kernel platform repos from bp scanning
 PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
@@ -7,7 +11,7 @@ PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
 # Allow vendor prebuilt repos to exclude themselves from bp scanning
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
 
-PRODUCT_BRAND ?= LineageOS
+PRODUCT_BRAND ?= MistOS
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PRODUCT_PROPERTIES += \
