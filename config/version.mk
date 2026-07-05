8346d618bea76fe1ab63340cfe0f23c3674c21cc
Zabuka_zuzu <zuzu950444@gmail.com>
2026-07-05 16:11:52 +0000

    vendor: revert back old gms

--- config/version.mk
+++ config/version.mk
@@ -46,9 +46,7 @@ endif
 # GMS
 WITH_GMS ?= false
 ifeq ($(WITH_GMS),true)
-    $(call inherit-product, vendor/pixel/gms/products/gms.mk)
-    $(call inherit-product, vendor/pixel/prebuilts/config.mk)
-    $(call inherit-product, vendor/pixel/sounds/products/sounds.mk)
+    $(call inherit-product, vendor/gms/products/gms.mk)
     MIST_PACKAGE_TYPE := GAPPS
 else
     MIST_PACKAGE_TYPE := VANILLA
